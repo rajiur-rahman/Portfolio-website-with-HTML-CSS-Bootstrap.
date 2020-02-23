60b371801273e6cfc61bfed0cb038f845af7d0dd
rajiur-rahman <rajiur.raju103@gmail.com>
2020-02-23 14:55:06 +0600

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$('#php').LineProgressbar({
+  percentage: 99,
+  fillBackgroundColor: '#1abc9c',
+  height: '15px',
+  radius: '20px'
+});
+$('#jquery').LineProgressbar({
+  percentage: 75,
+  fillBackgroundColor: '#e06304',
+  height: '15px',
+  radius: '20px'
+});
+$('#bootstrap').LineProgressbar({
+  percentage:  60,
+  fillBackgroundColor: '#16934a',
+  height: '15px',
+  radius: '20px'
+});
+$('#css3').LineProgressbar({
+  percentage: 80,
+  fillBackgroundColor: '#2563c6',
+  height: '15px',
+  radius: '20px'
+});
+// wow code for animation
+new WOW().init();
